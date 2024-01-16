f0343ed7182b6f2d0b72b74c4338d16b207b57cb
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-01-16 21:59:58 +0000

    function returns an array instead of console.log
    changed App.js to console.log the function to see if it still works

--- client/src/functions/SelectFiveRandomFromList.js
+++ client/src/functions/SelectFiveRandomFromList.js
@@ -2,8 +2,7 @@ export default function SelectFiveRandomFromList(listOfSongs) {
     const randomList = [];
     for (let i = 0; i < 5; i++) {
         const randomNum = Math.floor(Math.random() * 5);
-        console.log(randomNum)
         randomList.push(listOfSongs[randomNum])
     }
-    console.log(randomList)
+    return (randomList)
 }
