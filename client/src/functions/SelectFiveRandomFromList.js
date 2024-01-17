670644200d447129d43c0600b4220b2ca51e2f4e
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-01-17 21:35:55 +0000

    random number is now based on input array length

--- client/src/functions/SelectFiveRandomFromList.js
+++ client/src/functions/SelectFiveRandomFromList.js
@@ -1,7 +1,7 @@
 export default function SelectFiveRandomFromList(listOfSongs) {
     const randomList = [];
     for (let i = 0; i < 5; i++) {
-        const randomNum = Math.floor(Math.random() * 5);
+        const randomNum = Math.floor(Math.random() * listOfSongs.length);
         randomList.push(listOfSongs[randomNum])
     }
     return (randomList)
