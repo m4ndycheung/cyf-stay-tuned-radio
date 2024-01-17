c0db0b9e41038779ae5673a8296ff8d17d93b439
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-01-17 21:59:31 +0000

    included a second variable to set return amount

--- client/src/functions/SelectFiveRandomFromList.js
+++ client/src/functions/SelectFiveRandomFromList.js
@@ -1,6 +1,6 @@
-export default function SelectFiveRandomFromList(inputArray) {
+export default function SelectFiveRandomFromList(inputArray, amount) {
     const randomList = [];
-    for (let i = 0; randomList.length < 5; i++) {
+    for (let i = 0; randomList.length < amount; i++) {
         const randomNum = Math.floor(Math.random() * inputArray.length);
         if (!randomList.includes(inputArray[randomNum])) {
             randomList.push(inputArray[randomNum])
