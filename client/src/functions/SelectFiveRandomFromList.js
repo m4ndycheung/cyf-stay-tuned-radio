6851fd6bba48333977954c7e10f85e34fda191ec
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-01-17 21:57:48 +0000

    renamed input variable

--- client/src/functions/SelectFiveRandomFromList.js
+++ client/src/functions/SelectFiveRandomFromList.js
@@ -1,9 +1,9 @@
-export default function SelectFiveRandomFromList(listOfSongs) {
+export default function SelectFiveRandomFromList(inputArray) {
     const randomList = [];
     for (let i = 0; randomList.length < 5; i++) {
-        const randomNum = Math.floor(Math.random() * listOfSongs.length);
-        if (!randomList.includes(listOfSongs[randomNum])) {
-            randomList.push(listOfSongs[randomNum])
+        const randomNum = Math.floor(Math.random() * inputArray.length);
+        if (!randomList.includes(inputArray[randomNum])) {
+            randomList.push(inputArray[randomNum])
         }
     }
     return (randomList)
