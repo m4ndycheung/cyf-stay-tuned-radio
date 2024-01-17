73756ae2fa109d8bb165d65eb92506e9f7e0ec81
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-01-17 21:40:41 +0000

    loop now based on output array length instead of i

--- client/src/functions/SelectFiveRandomFromList.js
+++ client/src/functions/SelectFiveRandomFromList.js
@@ -1,6 +1,6 @@
 export default function SelectFiveRandomFromList(listOfSongs) {
     const randomList = [];
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; randomList.length < 5; i++) {
         const randomNum = Math.floor(Math.random() * listOfSongs.length);
         if (!randomList.includes(listOfSongs[randomNum])) {
             randomList.push(listOfSongs[randomNum])
