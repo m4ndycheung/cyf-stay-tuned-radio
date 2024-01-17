1ae8b1d553b8915ed7531c6dd29078bf8cf6b848
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-01-17 21:38:56 +0000

    output array updates only if item not in array

--- client/src/functions/SelectFiveRandomFromList.js
+++ client/src/functions/SelectFiveRandomFromList.js
@@ -2,7 +2,9 @@ export default function SelectFiveRandomFromList(listOfSongs) {
     const randomList = [];
     for (let i = 0; i < 5; i++) {
         const randomNum = Math.floor(Math.random() * listOfSongs.length);
-        randomList.push(listOfSongs[randomNum])
+        if (!randomList.includes(listOfSongs[randomNum])) {
+            randomList.push(listOfSongs[randomNum])
+        }
     }
     return (randomList)
 }
