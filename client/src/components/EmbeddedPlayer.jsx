b18cd72881f87fbf3d278da473f3551d8fbbd692
Irianni Munoz <82213740+munozirianni1988@users.noreply.github.com>
2024-01-20 20:42:48 +0000

    Update EmbeddedPlayer.jsx

--- client/src/components/EmbeddedPlayer.jsx
+++ client/src/components/EmbeddedPlayer.jsx
@@ -1,18 +1,56 @@
-function EmbeddedPlayer() {
+import { useEffect } from "react";
+
+const EmbeddedPlayer = () => {
+  useEffect(() => {
+    const loadSpotifyEmbed = () => {
+      const element = document.getElementById("embed-iframe");
+      const options = {
+        width: "100%",
+        height: "400",
+        uri: "spotify:playlist:2RoQkVgIhgQmCZadrMoDLd",
+      };
+
+      const callback = (EmbedController) => {
+        document.querySelectorAll(".tracks").forEach((track) => {
+          track.addEventListener("click", () => {
+            EmbedController.pause();
+            EmbedController.loadUri(track.dataset.spotifyId);
+          });
+        });
+      };
+
+      window.onSpotifyIframeApiReady = (IFrameAPI) => {
+        IFrameAPI.createController(element, options, callback);
+      };
+    };
+
+    // Loading the Spotify Embed API script
+    const script = document.createElement("script");
+    script.src = "https://open.spotify.com/embed/iframe-api/v1";
+    script.async = true;
+    script.onload = loadSpotifyEmbed;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+      delete window.onSpotifyIframeApiReady;
+    };
+  }, []);
+
   return (
-    <>
-      <iframe
-        style={{ borderRadius: 12 + "px" }}
-        src="https://open.spotify.com/embed/playlist/2RoQkVgIhgQmCZadrMoDLd?utm_source=generator&theme=0"
-        width="100%"
-        height="352"
-        frameBorder="0"
-        allowFullScreen=""
-        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-        loading="lazy"
-      ></iframe>
-    </>
+    <div>
+      <div className="playlist">
+        <span
+          className="playlist"
+          data-spotify-id="spotify:playlist:2RoQkVgIhgQmCZadrMoDLd"
+        >
+          My First Playlist
+        </span>
+      </div>
+
+      <div id="embed-iframe"></div>
+    </div>
   );
-}
+};
 
 export default EmbeddedPlayer;
