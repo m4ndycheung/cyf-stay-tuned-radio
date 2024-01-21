4f4fd79f60d6fc0c250d9a26f9e62b5baf333183
mandy <109825538+m4ndycheung@users.noreply.github.com>
2024-01-21 17:33:10 +0000

    Added variables needed for Auth Flow

--- server/server.js
+++ server/server.js
@@ -13,3 +13,11 @@ app.listen(PORT, () => {
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+
+const AUTH_URL = "https://accounts.spotify.com/authorize";
+const TOKEN = "https://accounts.spotify.com/api/token";
+const API_BASE_URL = "https://api.spotify.com/v1/";
