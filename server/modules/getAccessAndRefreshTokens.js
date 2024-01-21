350804a6a138f17c257ee3941f9c800ff113b503
mandy <109825538+m4ndycheung@users.noreply.github.com>
2024-01-21 21:47:20 +0000

    Created module to get tokens
    Tested /callback endpoint works

---
+++ server/modules/getAccessAndRefreshTokens.js
@@ -0,0 +1,5 @@
+const getAccessAndRefreshTokens = (req, res) => {
+  res.send("callback endpoint reached");
+};
+
+module.exports = getAccessAndRefreshTokens;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const initiateLogin = require("./modules/initiateLogin");
+const getAccessAndRefreshTokens = require("./modules/getAccessAndRefreshTokens");
 
 const PORT = process.env.PORT || 3001;
 
@@ -14,5 +15,5 @@ app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
-// add login function here
 app.get("/login", initiateLogin);
+app.get("/callback", getAccessAndRefreshTokens);
