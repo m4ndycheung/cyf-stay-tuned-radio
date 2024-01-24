5fedf93432e866510211fe820cfeec4d33225677
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-01-24 20:44:38 +0000

    include method and headers option in fetch call

--- server/modules/getTracksFromPlaylist.js
+++ server/modules/getTracksFromPlaylist.js
@@ -1,3 +1,8 @@
 const getTracksFromPlaylist = async function (access_token, playlist_id) {
-    const getTracksRequest = await fetch(`https://api.spotify.com/v1/playlists/${playlist_id}`);
+    const getTracksRequest = await fetch(`https://api.spotify.com/v1/playlists/${playlist_id}`, {
+        method: "GET",
+        headers: {
+          "Authorization": `Bearer ${access_token}`
+        }
+      });
   }
