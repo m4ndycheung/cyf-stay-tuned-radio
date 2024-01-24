dcc68052aa4db720540dcf5368ae42740e2cffa8
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-01-24 23:35:22 +0000

    import createDeleteTrackArray and passed array data

--- server/modules/deleteAllTracksFromSpotifyPlaylist.js
+++ server/modules/deleteAllTracksFromSpotifyPlaylist.js
@@ -1,5 +1,7 @@
 import getTracksFromPlaylist from "./getTracksFromPlaylist";
+import createDeleteTrackArray from "./createDeleteTrackArray";
 
 const deleteAllTracksFromSpotifyPlaylist = async function(access_token, track_id) {
     const playlistTrackArray = await getTracksFromPlaylist(access_token, track_id)
+    const deleteArray = createDeleteTrackArray(playlistTrackArray)
 }
