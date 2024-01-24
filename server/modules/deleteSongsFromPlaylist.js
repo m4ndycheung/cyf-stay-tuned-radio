e97dddc0d00305095e5471e357283b4ff56d4ed3
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-01-24 18:57:58 +0000

    include body in fetch call with tracks and snapshot_id

--- server/modules/deleteSongsFromPlaylist.js
+++ server/modules/deleteSongsFromPlaylist.js
@@ -1,9 +1,16 @@
-async function deleteSongsFromPlaylist(playlist_id, access_token) {
+async function deleteSongsFromPlaylist(playlist_id, access_token, snapshot_id, songsToDelURIArray) {
     data = await fetch(`https://api.spotify.com/v1/playlists/${playlist_id}/tracks`, {
         method: "DELETE",
         headers: {
             "Authorization": `Bearer ${access_token}`,
             "Content-Type": "application/json"
-        }
+        },
+        //body content needs to match Content-Type stated in the headers section
+        //tracks parameter is an array of objects which includes uri of the tracks (in form of {spotify:track:track_id})
+        //snapshot_id is a version identifier for playlists and changes with each modification
+        body: JSON.stringify({
+            tracks: songsToDelURIArray,
+            snapshot_id: snapshot_id
+        })
     })
 }
