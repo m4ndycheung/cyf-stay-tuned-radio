838558d8a07b437200616e903f080fb5af4afbf4
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-01-24 18:42:26 +0000

    added method and header options to fetch

--- server/modules/deleteSongsFromPlaylist.js
+++ server/modules/deleteSongsFromPlaylist.js
@@ -1,3 +1,9 @@
-async function deleteSongsFromPlaylist(playlist_id) {
-    data = await fetch(`https://api.spotify.com/v1/playlists/${playlist_id}/tracks`)
+async function deleteSongsFromPlaylist(playlist_id, access_token) {
+    data = await fetch(`https://api.spotify.com/v1/playlists/${playlist_id}/tracks`, {
+        method: "DELETE",
+        headers: {
+            "Authorization": `Bearer ${access_token}`,
+            "Content-Type": "application/json"
+        }
+    })
 }
