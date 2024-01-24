9cdc2ab128e15b6d6c917b4153f19b9c652cbb00
Irianni Munoz <82213740+munozirianni1988@users.noreply.github.com>
2024-01-24 22:02:38 +0000

    API call to db from server side
    Getting all the songs from the database on the server side

--- server/server.js
+++ server/server.js
@@ -1,19 +1,26 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const initiateLogin = require("./modules/initiateLogin");
 const getAccessAndRefreshTokens = require("./modules/getAccessAndRefreshTokens");
+const apiCallToGetSongsFromDb = require("./modules/apiCallToGetSongsFromDb");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.json());
+
+app.use(cors());
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
-});
+// app.get("/", function (req, res) {
+//   res.send("Hello World");
+// });
 
 app.get("/login", initiateLogin);
 app.get("/callback", getAccessAndRefreshTokens);
+app.get("/", apiCallToGetSongsFromDb);
