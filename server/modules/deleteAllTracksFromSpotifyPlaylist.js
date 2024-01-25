ec320c673068eec7cad49506674242f0f1c5cd01
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-01-25 00:45:41 +0000

    replaced import with require to load modules

--- server/modules/deleteAllTracksFromSpotifyPlaylist.js
+++ server/modules/deleteAllTracksFromSpotifyPlaylist.js
@@ -1,11 +1,15 @@
-import getPlaylistData from "./getPlaylistData";
-import createDeleteTrackArray from "./createDeleteTrackArray";
-import deleteTracksFromPlaylist from "./deleteTracksFromPlaylist";
+const getPlaylistData = require("./getPlaylistData");
+const createDeleteTrackArray = require("./createDeleteTrackArray");
+const deleteTracksFromPlaylist = require("./deleteTracksFromPlaylist");
 
 const deleteAllTracksFromSpotifyPlaylist = async function(access_token, playlist_id) {
-    const playlistData = await getTracksFromPlaylist(access_token, playlist_id)
+    const playlistData = await getPlaylistData(access_token, playlist_id)
     const snapshot_id = playlistData.snapshot_id
     const tracksArray = playlistData.tracks.items
     const deleteArray = createDeleteTrackArray(tracksArray)
     const deleteAllTracksRequest = await deleteTracksFromPlaylist(access_token, playlist_id, snapshot_id, deleteArray)
+    const deleteAllTracksResponse = await deleteAllTracksRequest.json()
+    return deleteAllTracksResponse
 }
+
+module.exports = deleteAllTracksFromSpotifyPlaylist
