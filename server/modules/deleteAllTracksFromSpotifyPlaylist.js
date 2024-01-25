262a141f847077cf49a24b040fbc7fdb4c6208de
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-01-25 01:01:43 +0000

    updated module name to plural

--- server/modules/deleteAllTracksFromSpotifyPlaylist.js
+++ server/modules/deleteAllTracksFromSpotifyPlaylist.js
@@ -1,12 +1,12 @@
 const getPlaylistData = require("./getPlaylistData");
-const createDeleteTrackArray = require("./createDeleteTrackArray");
+const createDeleteTracksArray = require("./createDeleteTracksArray");
 const deleteTracksFromPlaylist = require("./deleteTracksFromPlaylist");
 
 const deleteAllTracksFromSpotifyPlaylist = async function(access_token, playlist_id) {
     const playlistData = await getPlaylistData(access_token, playlist_id)
     const snapshot_id = playlistData.snapshot_id
     const tracksArray = playlistData.tracks.items
-    const deleteArray = createDeleteTrackArray(tracksArray)
+    const deleteArray = createDeleteTracksArray(tracksArray)
     const deleteAllTracksRequest = await deleteTracksFromPlaylist(access_token, playlist_id, snapshot_id, deleteArray)
     const deleteAllTracksResponse = await deleteAllTracksRequest.json()
     return deleteAllTracksResponse
