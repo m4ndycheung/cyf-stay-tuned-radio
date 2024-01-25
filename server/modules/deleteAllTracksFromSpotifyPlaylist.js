f04106c9e17432069707862559e5acbb004b48c1
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-01-25 00:19:42 +0000

    fixed typo for input variable

--- server/modules/deleteAllTracksFromSpotifyPlaylist.js
+++ server/modules/deleteAllTracksFromSpotifyPlaylist.js
@@ -2,7 +2,7 @@ import getPlaylistData from "./getPlaylistData";
 import createDeleteTrackArray from "./createDeleteTrackArray";
 import deleteTracksFromPlaylist from "./deleteTracksFromPlaylist";
 
-const deleteAllTracksFromSpotifyPlaylist = async function(access_token, track_id) {
-    const playlistData = await getTracksFromPlaylist(access_token, track_id)
+const deleteAllTracksFromSpotifyPlaylist = async function(access_token, playlist_id) {
+    const playlistData = await getTracksFromPlaylist(access_token, playlist_id)
     const deleteArray = createDeleteTrackArray(playlistData)
 }
