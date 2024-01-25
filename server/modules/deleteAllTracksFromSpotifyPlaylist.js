9572e507370934944b882c199b0811d6747ce31a
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-01-25 00:24:22 +0000

    created variables for snapshot_id and tracksArray

--- server/modules/deleteAllTracksFromSpotifyPlaylist.js
+++ server/modules/deleteAllTracksFromSpotifyPlaylist.js
@@ -4,5 +4,7 @@ import deleteTracksFromPlaylist from "./deleteTracksFromPlaylist";
 
 const deleteAllTracksFromSpotifyPlaylist = async function(access_token, playlist_id) {
     const playlistData = await getTracksFromPlaylist(access_token, playlist_id)
+    const snapshot_id = playlistData.snapshot_id
+    const tracksArray = playlistData.tracks.items
     const deleteArray = createDeleteTrackArray(playlistData)
 }
