c13c8d3910fe748ebcc842a078b9f8c959859248
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-01-26 19:23:07 +0000

    created and imported a simple component to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import EmbeddedPlayer from "./components/EmbeddedPlayer";
+import FormTextInput from "./components/FormTextInput";
 function App() {
   const [count, setCount] = useState(0);
 
@@ -17,6 +18,7 @@ function App() {
         </a>
       </div>
       <EmbeddedPlayer />
+      <FormTextInput />
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
---
+++ client/src/components/FormTextInput.jsx
@@ -0,0 +1,7 @@
+const FormTextInput = () => {
+    return (
+        <p>I am a FormTextInput Component</p>
+    )
+}
+
+export default FormTextInput
