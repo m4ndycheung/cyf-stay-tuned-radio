03c6fac258cf234da8e2814543df38cd39df56c4
mandy <109825538+m4ndycheung@users.noreply.github.com>
2024-01-26 22:21:19 +0000

    Added relevant request information
    Added access_token to Authorization

--- server/modules/addTracksToPlaylistRequest.js
+++ server/modules/addTracksToPlaylistRequest.js
@@ -1,6 +1,13 @@
 const addTracksToPlaylist = async function (access_token) {
   const addTracksRequest = await fetch(
-    `https://api.spotify.com/v1/playlists/7ML1iO1h2gFyjwYUkzcOGK/tracks`
+    `https://api.spotify.com/v1/playlists/7ML1iO1h2gFyjwYUkzcOGK/tracks`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${access_token}`,
+      },
+    }
   );
 };
 
