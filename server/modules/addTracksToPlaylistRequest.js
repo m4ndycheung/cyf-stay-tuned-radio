9e60c452950fece8122cf1057e580d20f0c6b830
mandy <109825538+m4ndycheung@users.noreply.github.com>
2024-01-26 22:22:20 +0000

    Added body containing Spotify track ids

--- server/modules/addTracksToPlaylistRequest.js
+++ server/modules/addTracksToPlaylistRequest.js
@@ -7,6 +7,15 @@ const addTracksToPlaylist = async function (access_token) {
         "Content-Type": "application/json",
         Authorization: `Bearer ${access_token}`,
       },
+      body: JSON.stringify({
+        uris: [
+          "spotify:track:7m9XR7FquXLP1FewdAcNS9",
+          "spotify:track:0Rr9HxnTEnYd8apQ4abMPx",
+          "spotify:track:5Tu7E5l3Czv6APtGkAsLv4",
+          "spotify:track:67MoDnJtywQsAS6Bovvuon",
+          "spotify:track:5gw8HNcrqliEw0X6pPrPvG",
+        ],
+      }),
     }
   );
 };
