626a9f38f0cb52a1a782693b9a52aa097280315b
mandy <109825538+m4ndycheung@users.noreply.github.com>
2024-01-26 22:15:39 +0000

    Added the API URL
    changed the name of the async function slightly

--- server/modules/addTracksToPlaylistRequest.js
+++ server/modules/addTracksToPlaylistRequest.js
@@ -1,3 +1,7 @@
-const addTracksToPlaylistRequest = async function (req, res) {};
+const addTracksToPlaylist = async function (access_token) {
+  const addTracksRequest = await fetch(
+    `https://api.spotify.com/v1/playlists/7ML1iO1h2gFyjwYUkzcOGK/tracks`
+  );
+};
 
-module.exports = addTracksToPlaylistRequest;
+module.exports = addTracksToPlaylist;
