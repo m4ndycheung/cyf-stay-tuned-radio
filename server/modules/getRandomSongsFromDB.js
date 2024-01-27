b5ce53ffb35b916eb9dbf4296dd443bbfaf5019d
mandy <109825538+m4ndycheung@users.noreply.github.com>
2024-01-27 17:57:03 +0000

    Deleted getRandomSongsFromDB.js

--- server/modules/getRandomSongsFromDB.js
+++
@@ -1,8 +0,0 @@
-const apiCall = require("./apiCallToGetSongsFromDb");
-
-const getRandomSongsFromDB = async function () {
-  const allSongs = await apiCall();
-  console.log(allSongs);
-};
-
-module.exports = getRandomSongsFromDB;
