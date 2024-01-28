78719a9fa725ba397f359c1f756cb4ddd3d81995
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-01-28 18:03:49 +0000

    fetch /database/songs and store response as songsData

--- server/modules/apiCallToGetRandomSongsFromDB.js
+++ server/modules/apiCallToGetRandomSongsFromDB.js
@@ -1,47 +1,8 @@
-const { Pool } = require("pg");
+const apiCallToGetRandomSongsFromDB = async function(req, res) {
+    const server_url = process.env.server_url
+    const requestSongs = await fetch(`${server_url}/database/songs`)
+    const songsData = await requestSongs.json()
+    res.send(songsData)
+}
 
-const db = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  ssl: true,
-});
-
-const apiCallToGetRandomSongsFromDB = function (req, res) {
-  db.query("SELECT * FROM tracks")
-    .then((result) => {
-      const data = result.rows;
-
-      const selectRandomFromList = function (inputArray, amount) {
-        const randomList = [];
-        for (let i = 0; randomList.length < amount; i++) {
-          const randomNum = Math.floor(Math.random() * inputArray.length);
-          if (!randomList.includes(inputArray[randomNum])) {
-            randomList.push(inputArray[randomNum]);
-          }
-        }
-
-        //  The array should look like this:
-        //  ["Spotify:track:4iV5W9uYEdYUVa79Axb7Rh","spotify:track:1301WleyT98MSxVHPZCA6M"]
-        const trackIDsToAddArray = [];
-
-        randomList.map((item) => {
-          const newString = `Spotify:track:${item.spotify_song_id}`;
-          trackIDsToAddArray.push(newString);
-        });
-        console.log(`hello`);
-        console.log(trackIDsToAddArray);
-        return trackIDsToAddArray;
-      };
-
-      selectRandomFromList(data, 3);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Internal Server Error");
-    });
-};
-
-module.exports = apiCallToGetRandomSongsFromDB;
+module.exports = apiCallToGetRandomSongsFromDB
