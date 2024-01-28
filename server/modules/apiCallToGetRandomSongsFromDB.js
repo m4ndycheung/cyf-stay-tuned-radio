62a6d00e74132bef297ed79fdf6204eb087ef61f
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-01-28 18:12:49 +0000

    added comments to explain initial fetch call to get songs data

--- server/modules/apiCallToGetRandomSongsFromDB.js
+++ server/modules/apiCallToGetRandomSongsFromDB.js
@@ -1,7 +1,9 @@
 const apiCallToGetRandomSongsFromDB = async function(req, res) {
     const server_url = process.env.server_url
+    // waits for a fetch call to the database to be completed
     const requestSongs = await fetch(`${server_url}/songs`)
-    const songsData = await requestSongs.json()
+    // waits for response of the fetch call above and stores the data (songs array) in an array called songsData
+    const songsData = await requestSongs.json() 
     res.send(songsData)
 }
 
