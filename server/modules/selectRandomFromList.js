488abe8ec0bd2f38c6b57c41c42521dd8de3184d
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-01-28 12:28:11 +0000

    Revert "Deleted selectRandomFromList.js"
    This reverts commit d7b9aa26254e2207929cb58a1b11d71788d8c73c.

---
+++ server/modules/selectRandomFromList.js
@@ -0,0 +1,12 @@
+const selectRandomFromList = function (inputArray, amount) {
+  const randomList = [];
+  for (let i = 0; randomList.length < amount; i++) {
+    const randomNum = Math.floor(Math.random() * inputArray.length);
+    if (!randomList.includes(inputArray[randomNum])) {
+      randomList.push(inputArray[randomNum]);
+    }
+  }
+  return randomList;
+};
+
+module.exports = selectRandomFromList;
