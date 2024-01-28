e21c01508c23da91a540bf6d5695af78a5cc010d
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-01-28 17:01:18 +0000

    Create apiCallToGetSongsFromDb.js

---
+++ server/modules/apiCallToGetSongsFromDb.js
@@ -0,0 +1,24 @@
+const { Pool } = require("pg");
+
+const db = new Pool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  ssl: true,
+});
+
+const apiCallToGetSongsFromDB = function (req, res) {
+  db.query("SELECT * FROM tracks")
+    .then((result) => {
+      console.log(result.rows);
+      res.status(200).json(result.rows);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
+    });
+};
+
+module.exports = apiCallToGetSongsFromDB;
