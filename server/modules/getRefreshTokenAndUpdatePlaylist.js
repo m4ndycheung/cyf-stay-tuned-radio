74f97bb3ba9de9854166c4ae1e73ff1cde089e06
christina-mifsud <christina.mifsud@gmail.com>
2024-01-29 23:42:48 +0000

    delete all items from spotify playlist

--- server/modules/getRefreshTokenAndUpdatePlaylist.js
+++ server/modules/getRefreshTokenAndUpdatePlaylist.js
@@ -1,13 +1,19 @@
 var request = require("request");
 const { Pool } = require("pg");
-const exchangeRefreshForAccessToken = require("./exchangeRefreshForAccessToken");
+const deleteAllTracksFromSpotifyPlaylist = require("./deleteAllTracksFromSpotifyPlaylist");
 
+// /update route
 const getRefreshAndUpdatePlaylist = async function (req, res) {
+  // calling the /refresh_token endpoint to call the function to get the access and refresh tokens
   const serverURL = process.env.server_url;
-
   const requestRefreshToken = await fetch(`${serverURL}/refresh_token`);
   const newTokenObject = await requestRefreshToken.json();
-  console.log(`NEW access token: ${newTokenObject.access_token}`);
+  const newAccessToken = newTokenObject.access_token;
+  console.log(`NEWWWWWWWWW access token: ${newAccessToken}`);
+
+  // API call to delete tracks from yesterday's spotify playlist
+  const playlistID = process.env.playlist_id;
+  deleteAllTracksFromSpotifyPlaylist(newAccessToken, playlistID);
 };
 
 module.exports = getRefreshAndUpdatePlaylist;
