66b60a17291afe8bfd7a5fe9bd464dfe6a7581f4
christina-mifsud <christina.mifsud@gmail.com>
2024-01-29 23:43:19 +0000

    Add tracks to playlist

--- server/modules/getRefreshTokenAndUpdatePlaylist.js
+++ server/modules/getRefreshTokenAndUpdatePlaylist.js
@@ -1,6 +1,7 @@
 var request = require("request");
 const { Pool } = require("pg");
 const deleteAllTracksFromSpotifyPlaylist = require("./deleteAllTracksFromSpotifyPlaylist");
+const addTracksToPlaylist = require("./addTracksToPlaylist");
 
 // /update route
 const getRefreshAndUpdatePlaylist = async function (req, res) {
@@ -14,6 +15,11 @@ const getRefreshAndUpdatePlaylist = async function (req, res) {
   // API call to delete tracks from yesterday's spotify playlist
   const playlistID = process.env.playlist_id;
   deleteAllTracksFromSpotifyPlaylist(newAccessToken, playlistID);
+
+  // API call to add tracks randomly selected from db to spotify playlist
+  const getRandomSongs = await fetch(`${serverURL}/songs/random`);
+  const randomSongs = await getRandomSongs.json();
+  addTracksToPlaylist(newAccessToken, randomSongs, playlistID);
 };
 
 module.exports = getRefreshAndUpdatePlaylist;
