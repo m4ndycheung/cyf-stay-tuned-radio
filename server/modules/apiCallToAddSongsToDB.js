1a9dc61d51df52a839215dd9338764b3a6b642fe
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-02-01 01:54:30 +0000

    added { Pool } as requirement and setup db details

--- server/modules/apiCallToAddSongsToDB.js
+++ server/modules/apiCallToAddSongsToDB.js
@@ -1,4 +1,15 @@
+const { Pool } = require("pg");
+
 const apiCallToAddSongsToDB = function(req, res) {
+    const db = new Pool({
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        database: process.env.DB_NAME,
+        ssl: true,
+      });
+
     console.log(req.body)
     res.send(req.body)
 }
