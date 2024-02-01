33778e37270fba28bfad60617114e729e6874057
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-02-01 02:24:53 +0000

    created a regex to parse spotify id from url

--- server/modules/apiCallToAddSongsToDB.js
+++ server/modules/apiCallToAddSongsToDB.js
@@ -8,14 +8,16 @@ const apiCallToAddSongsToDB = function(req, res) {
         port: process.env.DB_PORT,
         database: process.env.DB_NAME,
         ssl: true,
-      });
-
-      const artist = req.body.artist
-      const song_title = req.body.song_title
-      const spotify_url = req.body.spotify_url
-
+    });
+    
+    const artist = req.body.artist
+    const song_title = req.body.song_title
+    const spotify_url = req.body.spotify_url
+    const re = /(?!track\/)([a-zA-Z0-9]){22}/
+    const spotify_song_id = re.exec(spotify_url)[0]
+    console.log(spotify_song_id)
     console.log(artist, song_title, spotify_url)
-    res.send(artist, song_title, spotify_url)
+    res.send(spotify_song_id)
 }
 
 module.exports = apiCallToAddSongsToDB
