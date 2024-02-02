e474f5c983327ee3715788b51bf7cd9be1f0a5ac
mandy <109825538+m4ndycheung@users.noreply.github.com>
2024-02-02 22:50:40 +0000

    Copied access token code
    So I can get an access token for the api call

--- server/modules/spotifySearch.js
+++ server/modules/spotifySearch.js
@@ -1,5 +1,13 @@
-const searchForSongsOnSpotify = async function (req, res) {
-  res.send("Porridge");
-};
+const querystring = require("querystring");
 
+const searchForSongsOnSpotify = async function (req, res) {
+  //calling the /refresh_token endpoint to call the function to get the access and refresh tokens
+  const serverURL = process.env.server_url;
+  const requestRefreshToken = await fetch(`${serverURL}/refresh_token`);
+  const newTokenObject = await requestRefreshToken.json();
+  const newAccessToken = newTokenObject.access_token;
+  //   console.log(`New access token: ${newAccessToken}`);
+
+  res.send(newAccessToken);
+};
 module.exports = searchForSongsOnSpotify;
