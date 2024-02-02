05c30768064f98349888d9e7010535e9fe15c8a7
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-02-02 20:21:39 +0000

    wrapped form components inside div and set class

--- client/src/components/FormTextInput.jsx
+++ client/src/components/FormTextInput.jsx
@@ -2,10 +2,21 @@ import "./FormTextInput.css";
 
 const FormTextInput = ({ inputName, inputID, handleChangeEventFormInput }) => {
     return (
-    <>
-        <label for={inputID} className="form_input_label">{inputName}: </label>
-        <input type="text" id={inputID} className="form_input_element" placeholder={`Enter ${inputName}`} onChange={() => handleChangeEventFormInput(event.target.id, event.target.value)}/>
-    </>
+        <div className="form-group">
+            <label
+                for={inputID}
+                className="form_input_label"
+            >
+                {inputName}: 
+            </label>
+            <input
+                type="text"
+                id={inputID}
+                className="form_input_element form-control"
+                placeholder={`Enter ${inputName}`}
+                onChange={() => handleChangeEventFormInput(event.target.id, event.target.value)}
+            />
+        </div>
     )
 }
 
