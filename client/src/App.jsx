3dcbcc0041dae06f49db333a1d1c803ddf727531
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-02-02 20:16:14 +0000

    import bootstrap and components into App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 import EmbeddedPlayer from "./components/EmbeddedPlayer";
 import AddSongForm from "./components/AddSongForm";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
 
 function App() {
   return (
     <>
+      <Header />
+      <Home />
+      <About />
       <EmbeddedPlayer />
       <AddSongForm />
     </>
