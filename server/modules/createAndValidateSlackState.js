e3405028ba4b46ae8bc2d1254f1b94eebd06f381
mandy <109825538+m4ndycheung@users.noreply.github.com>
2024-02-02 16:33:38 +0000

    Added async to functions and tidied console.logz

--- server/modules/createAndValidateSlackState.js
+++ server/modules/createAndValidateSlackState.js
@@ -6,15 +6,15 @@ const activeID = {}; //here i'm creating an empty object to store the id
 function createIdentifier() {
   //this generates a unique id, save it on the object with the time
   //plus 10 mins, as a key value pair.
-  function generate() {
-    console.log(`activeID state store generate: ${JSON.stringify(activeID)}`);
+  async function generate() {
     const newValue = uuid.v4();
     activeID[newValue] = Date.now() + 10 * 60 * 1000; // 10 minutes
     return newValue;
   }
 
   //this is checking if the id is valid or if expired
-  function validate(state) {
+  async function validate(state) {
+    // Checking activeID is storing the right data
     console.log(`activeID state store: ${JSON.stringify(activeID)}`);
     const expiresAt = activeID[state];
     if (expiresAt && Date.now() <= expiresAt) {
