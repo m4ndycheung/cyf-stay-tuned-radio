49a8b1f2c427982ad78c0234758c157a0195452c
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-02-02 15:30:47 +0000

    assign class names to label and input element

--- client/src/components/FormTextInput.jsx
+++ client/src/components/FormTextInput.jsx
@@ -3,8 +3,8 @@ import "./FormTextInput.css";
 const FormTextInput = ({ inputName, inputID, handleChangeEventFormInput }) => {
     return (
     <>
-        <label for={inputID}>{inputName}: </label>
-        <input type="text" id={inputID} onChange={() => handleChangeEventFormInput(event.target.id, event.target.value)}/>
+        <label for={inputID} className="form_input_label">{inputName}: </label>
+        <input type="text" id={inputID} className="form_input_element" onChange={() => handleChangeEventFormInput(event.target.id, event.target.value)}/>
     </>
     )
 }
