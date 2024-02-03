61ac87c25908ceb8ad2b0c49ad4787678cedccec
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-02-03 17:48:04 +0000

    remove css dependancy in Header  component

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,7 +1,6 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import "./header.css"
 
 const Header = () => {
     return (
