e56e420cb309ed50bd7285d3550655e460551f7f
mandy <109825538+m4ndycheung@users.noreply.github.com>
2024-02-04 17:18:54 +0000

    Changed back to render target

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// http://localhost:3001
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3001",
+        target: "https://stay-tuned-radio-server.onrender.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
