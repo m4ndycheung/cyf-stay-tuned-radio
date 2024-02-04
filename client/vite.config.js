822909245c8fbe468e74d64b5dcf9959a3f10c11
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-02-04 15:03:35 +0000

    create variable and set to VITE_SERVER_URL

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const url = import.meta.env.VITE_SERVER_URL
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: import.meta.env.VITE_SERVER_URL,
+        target: url,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
