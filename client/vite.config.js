5f8de98a323574fbec03366d751c415901f152d7
mandy <109825538+m4ndycheung@users.noreply.github.com>
2024-02-04 17:47:22 +0000

    Reinstated original code for vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,18 +3,15 @@ import react from "@vitejs/plugin-react";
 
 // http://localhost:3001
 // https://vitejs.dev/config/
-
-// "/api": {
-//         target: "https://stay-tuned-radio-server.onrender.com",
-//         changeOrigin: true,
-//         rewrite: (path) => path.replace(/^\/api/, ""),
-//       },
-
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      api: "https://stay-tuned-radio-server.onrender.com",
+      "/api": {
+        target: "https://stay-tuned-radio-server.onrender.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
     },
   },
 });
