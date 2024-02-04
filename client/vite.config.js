4dfb997ddebbc307bf74dba023a7626c963ac6ef
mandy <109825538+m4ndycheung@users.noreply.github.com>
2024-02-04 17:33:28 +0000

    Trying a shorter line

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,15 +3,18 @@ import react from "@vitejs/plugin-react";
 
 // http://localhost:3001
 // https://vitejs.dev/config/
+
+// "/api": {
+//         target: "https://stay-tuned-radio-server.onrender.com",
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, ""),
+//       },
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": {
-        target: "https://stay-tuned-radio-server.onrender.com",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
+      api: "https://stay-tuned-radio-server.onrender.com",
     },
   },
 });
