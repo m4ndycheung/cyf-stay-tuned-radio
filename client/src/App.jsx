317cf0c5f32e908a3f27cf4292c6962c697cbce9
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-02-07 18:32:13 +0000

    create login/logout button
    set sessionStorage
    get and set token to params querystring

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,37 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import EmbeddedPlayer from "./components/EmbeddedPlayer";
 import AddSongForm from "./components/AddSongForm";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
-import Background from './components/Background';
+import Background from "./components/Background";
+import { useEffect, useState } from "react";
 
 function App() {
+  const urlSearchString = window.location.search;
+  const params = new URLSearchParams(urlSearchString);
+  const token = params.get(`token`);
+  const [tokenData, setTokenData] = useState(token);
+
+  useEffect(() => {
+    if (token !== null) {
+      sessionStorage.setItem("token", token);
+    }
+    console.log(sessionStorage.getItem("token"));
+  }, [token, tokenData]);
+
+  const handleOnClickLogOut = (event) => {
+    event.preventDefault();
+    sessionStorage.removeItem("token");
+    setTokenData(null);
+  };
+
+  const handleOnClickLogin = (event) => {
+    event.preventDefault();
+    window.open("http://localhost:5173/api/jwt/authorise", "_self");
+  };
+
   return (
     <>
       <Background />
@@ -15,7 +39,21 @@ function App() {
       {/* <Home />
       <About /> */}
       <EmbeddedPlayer />
-      <AddSongForm />
+      {sessionStorage.getItem("token") !== null ? (
+        <>
+          <AddSongForm />
+          <button
+            className="btn btn-secondary my-2"
+            onClick={handleOnClickLogOut}
+          >
+            Log Out
+          </button>
+        </>
+      ) : (
+        <button className="btn btn-primary" onClick={handleOnClickLogin}>
+          Login
+        </button>
+      )}
     </>
   );
 }
