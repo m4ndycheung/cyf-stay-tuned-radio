1f0d54b0697ed16418d3e63b10a7bc611ba0e76e
mandy <109825538+m4ndycheung@users.noreply.github.com>
2024-02-08 20:21:11 +0000

    Verifies jwt valid, error message too

--- server/modules/authorisation/verifyMaciToken.js
+++ server/modules/authorisation/verifyMaciToken.js
@@ -1,7 +1,18 @@
 const jwt = require("jsonwebtoken");
 
 function verifyMaciToken(req, res, next) {
-  console.log("verified a token cake");
+  // get authorization part of header
+  const authorisationHeader = req.headers.authorization;
+
+  // remove Bearer to only get the JWT
+  const maciToken = authorisationHeader.replace("Bearer ", "");
+
+  // verify the token was created with maci secret
+  const decode = jwt.verify(maciToken, process.env.JWT_SECRET, (error) => {
+    if (error) return res.send("Hasta la vista, Aby! (i.e. jwt not verified)");
+  });
+
+  console.log(decode);
   next();
 }
 
