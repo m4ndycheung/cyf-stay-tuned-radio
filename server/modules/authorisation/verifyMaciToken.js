5bcf2d3c3d1b6c83a0ba3876e2ccdfa3b6e5ef9e
mandy <109825538+m4ndycheung@users.noreply.github.com>
2024-02-08 21:39:16 +0000

    Validation for verifying maciToken

--- server/modules/authorisation/verifyMaciToken.js
+++ server/modules/authorisation/verifyMaciToken.js
@@ -1,19 +1,35 @@
 const jwt = require("jsonwebtoken");
 
 function verifyMaciToken(req, res, next) {
-  // get authorization part of header
-  const authorisationHeader = req.headers.authorization;
+  const requestURL = req.url;
+  console.log(requestURL);
 
-  // remove Bearer to only get the JWT
-  const maciToken = authorisationHeader.replace("Bearer ", "");
+  const publicURLs = ["/slack-sign-in", "/slack/oauth_redirect", "/callback"];
 
-  // verify the token was created with maci secret
-  const decode = jwt.verify(maciToken, process.env.JWT_SECRET, (error) => {
-    if (error) return res.send("Hasta la vista, Aby! (i.e. jwt not verified)");
-  });
+  // if requestURL is not inside publicURLs then run following code
+  if (!publicURLs.includes(requestURL)) {
+    // get authorization part of header
+    const authorisationHeader = req.headers.authorization;
 
-  console.log(decode);
-  next();
+    // checks if authorisationHeader is not empty i.e undefined
+    if (authorisationHeader !== undefined) {
+      // remove Bearer to only get the JWT
+      const maciToken = authorisationHeader.replace("Bearer ", "");
+      // verify the token was created with maci secret
+      const decode = jwt.verify(maciToken, process.env.JWT_SECRET, (error) => {
+        // if can't verify, send error message
+        if (error)
+          return res.send("Hasta la vista, Aby! (i.e. jwt not verified)");
+      });
+
+      console.log(decode);
+      next();
+    } else {
+      res.send("no header");
+    }
+  } else {
+    next();
+  }
 }
 
 module.exports = verifyMaciToken;
