1e5d651f5f75269cdde5563e088a10dd35e4e4c4
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-02-10 01:42:51 +0000

    updated check to use regex instead of .includes

--- server/modules/authorisation/verifyMaciToken.js
+++ server/modules/authorisation/verifyMaciToken.js
@@ -12,11 +12,14 @@ function verifyMaciToken(req, res, next) {
     "/search",
   ];
 
-  const basicURLs = ["/songs/add"];
-  const adminURLs = ["/update"];
+  // const basicURLs = ["/songs/add"];
+  // const adminURLs = ["/update"];
+  //regex patterns to check if url starts with /update or /songs/add. regex is used so that if a / or ? is included at the end of the url, it will still be blocked unlike using array.includes where the url must match exactly
+  const basicURLsRE = /^\/songs\/add/;
+  const adminURLsRE = /^\/update/;
 
   // if requestURL is not inside publicURLs then run following code
-  if (basicURLs.includes(requestURL) || adminURLs.includes(requestURL)) {
+  if (basicURLsRE.test(requestURL) || adminURLsRE.test(requestURL)) {
     // get authorization part of header
     const authorisationHeader = req.headers.authorization;
 
@@ -36,7 +39,7 @@ function verifyMaciToken(req, res, next) {
         next();
       }
 
-      if (user_role === "basic" && basicURLs.includes(requestURL)) {
+      if (user_role === "basic" && basicURLsRE.test(requestURL)) {
         next();
       } else {
         res.send("you are not authorised");
