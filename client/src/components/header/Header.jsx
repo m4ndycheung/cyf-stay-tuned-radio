fa110e80a301dd6e1b4b39f3cbf9d650a70e762d
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-02-10 10:58:34 +0000

    added check for token and show login/logout
    add env for VITE_FRONTEND_URL

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,23 +1,50 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import { useEffect, useState } from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 const Header = () => {
-    return (
-        <Navbar expand="lg" className="rounded bg-body-tertiary mb-3">
-            <Container className='mx-3'>
-                <Navbar.Brand href="#home">MACI</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#about">About</Nav.Link>
-                        <Nav.Link href="#login">Login</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    )
-}
+  const server_url = import.meta.env.VITE_SERVER_URL;
+  const frontend_url = import.meta.env.VITE_FRONTEND_URL;
 
-export default Header
+  const [isLoggedIn, setLogin] = useState(false);
+  useEffect(() => {
+    if (sessionStorage.getItem("maciToken") !== null) setLogin(true);
+  }, []);
+
+  const handleLogout = function (event) {
+    event.preventDefault();
+    sessionStorage.clear();
+    window.open(`${frontend_url}`, "_self");
+  };
+
+  const handleLogin = function (event) {
+    event.preventDefault();
+    window.open(`${server_url}/slack-sign-in`, "_self");
+  };
+
+  return (
+    <Navbar expand="lg" className="rounded bg-body-tertiary mb-3">
+      <Container className="mx-3">
+        <Navbar.Brand href="#home">MACI</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            {isLoggedIn ? (
+              <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+            ) : (
+              <Nav.Link onClick={handleLogin}>Login</Nav.Link>
+            )}
+            {/* <Nav.Link onClick={open(isLoggedIn.link)}>
+              {isLoggedIn.name}
+            </Nav.Link> */}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
