ee1f9f08092b661053a94268f808433ec42e9106
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-02-10 16:54:07 +0000

    testing song id extraction from Spotify url

---
+++ server/spotify/util.js
@@ -0,0 +1,7 @@
+function extractSpotifyId(url) {
+  const re = /(?!track\/)([a-zA-Z0-9]){22}/;
+  const spotify_song_id = re.exec(url)[0];
+  return spotify_song_id;
+}
+
+module.exports = extractSpotifyId;
