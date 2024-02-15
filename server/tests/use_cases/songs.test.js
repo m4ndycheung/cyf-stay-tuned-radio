2a2e352b697238fa6ae83e5f1bfa32bd1a928435
Irianni Munoz <82213740+munozirianni1988@users.noreply.github.com>
2024-02-15 21:11:58 +0000

    setting test to check if song was added to DB

--- server/tests/use_cases/songs.test.js
+++ server/tests/use_cases/songs.test.js
@@ -1,21 +1,20 @@
 const request = require("supertest");
 const app = require("../../server.js");
+const db = require("../../data/database.js");
+const { expect } = require("code");
 
-test("expect true to be false", () => {
-  request(app)
+test("expect true to be false", async () => {
+  const result = await request(app)
     .post("/songs/add")
     .send({
       artist: "john",
       song_title: "My sad boring name",
-      spotify_url: "https://open.spotify.com/track/6FHUBs8P5qcjpj7C2QHdEq",
+      spotify_url: "https://open.spotify.com/track/5ChkMS8OtdzJeqyybCc9R5",
     })
-    .set("Accept", "application/json")
-    .expect("Content-Type", /json/)
-    .expect(200)
-    .end(function (err, res) {
-      if (err) {
-        // console.log(err);
-        console.log(res);
-      }
-    });
+    .set("Accept", "application/json");
+  expect(result.statusCode).equal(201);
+  const databaseResult = await db.query(
+    "SELECT song_name, artist_name, spotify_song_id FROM tracks WHERE spotify_song_id = '5ChkMS8OtdzJeqyybCc9R5' "
+  );
+  expect(databaseResult.rowCount).equal(1);
 });
