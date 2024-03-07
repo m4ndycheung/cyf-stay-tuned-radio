a216f2fe69bba5d9a18bf0e6d99581e37ce2d38a
mandy <109825538+m4ndycheung@users.noreply.github.com>
2024-03-07 19:40:53 +0000

    Added 10 starter songs to tracks table

--- server/data/spotify_tables.sql
+++ server/data/spotify_tables.sql
@@ -14,3 +14,14 @@ CREATE TABLE refresh_tokens_table (
   id                    SERIAL PRIMARY KEY,
   refresh_token         VARCHAR NOT NULL
 );
+
+INSERT INTO tracks (song_name, artist_name, spotify_song_id) VALUES ('Private Presley', 'Peach Pit', '40d4m1eI0erCRKLXz55RtT' );
+INSERT INTO tracks (song_name, artist_name, spotify_song_id) VALUES ('Concerning Hobbits', 'Howard Shore', '644es5aYPJghtZLjM1rmSP' );
+INSERT INTO tracks (song_name, artist_name, spotify_song_id) VALUES ('Machinist', 'Japanese Breakfast', '13FGWUlqQpGugvEcnEUqou' );
+INSERT INTO tracks (song_name, artist_name, spotify_song_id) VALUES ('Not Strong Enough', 'boygenius', '09DR0sHnQUhHOiSNttc1mv' );
+INSERT INTO tracks (song_name, artist_name, spotify_song_id) VALUES ('This Must Be the Place (Naive Melody) - 2005 Remaster', 'Talking Heads', '6aBUnkXuCEQQHAlTokv9or' );
+INSERT INTO tracks (song_name, artist_name, spotify_song_id) VALUES ('Helena', 'My Chemical Romance', '5dTHtzHFPyi8TlTtzoz1J9' );
+INSERT INTO tracks (song_name, artist_name, spotify_song_id) VALUES ('Going Under', 'Evanescence', '3UygY7qW2cvG9Llkay6i1i' );
+INSERT INTO tracks (song_name, artist_name, spotify_song_id) VALUES ('What Hurts The Most', 'Cascada', '2rVa6UVT2f95l3bGcMGCWQ' );
+INSERT INTO tracks (song_name, artist_name, spotify_song_id) VALUES ('Little Wing', 'Jimi Hendrix', '1Eolhana7nKHYpcYpdVcT5' );
+INSERT INTO tracks (song_name, artist_name, spotify_song_id) VALUES ('Everywhere', 'Fleetwood Mac', '1prZ0pr6XoRCxcrC3MCL0M' );
