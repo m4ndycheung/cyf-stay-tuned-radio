355ca65bd93fd7fbce50ec6544159de452b0a610
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-03-10 16:53:35 +0000

    wrap jwt token handling inside existing useEffect

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,19 +8,19 @@ import Background from "./components/Background";
 import UpdateSpotifyPlaylistButton from "./components/UpdateSpotifyPlaylistButton";
 
 function App() {
-  //getting token from query string sent from backend
-  const search = window.location.search;
-  const params = new URLSearchParams(search);
-  const maciToken = params.get("token");
-  // check if query return equals null and if it does not, set sessionstorage to maciToken
-  if (maciToken !== null) {
-    //set session storage data for maciToken
-    sessionStorage.setItem("maciToken", maciToken);
-    console.log(`sessionStorageData: ${sessionStorage.getItem("maciToken")}`);
-  }
-
   const [isLoggedIn, setLogin] = useState(false);
+
   useEffect(() => {
+    //getting token from query string sent from backend
+    const search = window.location.search;
+    const params = new URLSearchParams(search);
+    const maciToken = params.get("token");
+    // check if query return equals null and if it does not, set sessionstorage to maciToken
+    if (maciToken !== null) {
+      //set session storage data for maciToken
+      sessionStorage.setItem("maciToken", maciToken);
+      console.log(`sessionStorageData: ${sessionStorage.getItem("maciToken")}`);
+    }
     if (sessionStorage.getItem("maciToken") !== null) setLogin(true);
   }, []);
 
