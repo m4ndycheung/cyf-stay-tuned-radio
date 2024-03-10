c97314b61488f25ad60718745a8442d6b689a2e3
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-03-10 17:27:36 +0000

    create GET call to /update endpoint with auth header

--- client/src/components/UpdateSpotifyPlaylistButton.jsx
+++ client/src/components/UpdateSpotifyPlaylistButton.jsx
@@ -1,6 +1,15 @@
 const UpdateSpotifyPlaylistButton = () => {
-  function handleClickSpotifyUpdate() {
-    console.log("Clicked");
+  const backend_server = import.meta.env.VITE_SERVER_URL;
+
+  async function handleClickSpotifyUpdate() {
+    const updateSpotifyRequest = await fetch(`${backend_server}/update`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${sessionStorage.getItem("maciToken")}`,
+      },
+    });
+    const updateSpotifyResponse = await updateSpotifyRequest.json();
+    alert(updateSpotifyResponse.message);
   }
   return (
     <>
