bd13aa222601aadf4cc53fc03bc9e744f4ac107e
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-03-13 00:04:32 +0000

    fetch /login on click with authorisation in header

--- client/src/components/SpotifyFirstRunButton.jsx
+++ client/src/components/SpotifyFirstRunButton.jsx
@@ -1,10 +1,22 @@
 const SpotifyFirstRunButton = () => {
-  function handleClickSpotifyFirstRun() {
-    alert("Clicked");
+  const backend_server = import.meta.env.VITE_SERVER_URL;
+
+  async function handleClickSpotifyFirstRun() {
+    const getSpotifyAccessAndRefreshToken = await fetch(
+      `${backend_server}/login`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${sessionStorage.getItem("maciToken")}`,
+        },
+      }
+    );
+    const updateTokenResponse = await getSpotifyAccessAndRefreshToken.json();
+    alert(updateTokenResponse.message);
   }
   return (
     <button className="btn btn-primary" onClick={handleClickSpotifyFirstRun}>
-      Button
+      Get Access and Refresh Token
     </button>
   );
 };
