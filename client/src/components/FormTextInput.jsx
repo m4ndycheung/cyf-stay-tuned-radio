90154bf9cf0ba3d49d85d3513460339d04525155
mandy <109825538+m4ndycheung@users.noreply.github.com>
2024-03-15 22:51:26 +0000

    Removed event.target.id
    As I was not using handleChangeEventFormInput function which took a key, and value as arguments, I needed to remove event.target.id for now.

--- client/src/components/FormTextInput.jsx
+++ client/src/components/FormTextInput.jsx
@@ -1,23 +1,23 @@
 import "./FormTextInput.css";
 
 const FormTextInput = ({ inputName, inputID, handleChangeEventFormInput }) => {
-    return (
-        <div className="form-group">
-            <label
-                for={inputID}
-                className="form_input_label"
-            >
-                {inputName}: 
-            </label>
-            <input
-                type="text"
-                id={inputID}
-                className="form_input_element form-control"
-                placeholder={`Enter ${inputName}`}
-                onChange={() => handleChangeEventFormInput(event.target.id, event.target.value)}
-            />
-        </div>
-    )
-}
+  return (
+    <div className="form-group">
+      <label htmlFor={inputID} className="form_input_label">
+        {inputName}:
+      </label>
+      <input
+        type="text"
+        id={inputID}
+        className="form_input_element form-control"
+        placeholder={`Enter ${inputName}`}
+        onChange={(event) => {
+          console.log(event.target.value);
+          handleChangeEventFormInput(event.target.value);
+        }}
+      />
+    </div>
+  );
+};
 
-export default FormTextInput
+export default FormTextInput;
