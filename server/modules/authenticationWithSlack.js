fc231f08de24cc26556e0d46abea7d5149a1783e
mandy <109825538+m4ndycheung@users.noreply.github.com>
2024-03-15 20:38:39 +0000

    commented out console log

--- server/modules/authenticationWithSlack.js
+++ server/modules/authenticationWithSlack.js
@@ -9,7 +9,7 @@ const myIdentifier = createIdentifier();
 
 const authenticationWithSlack = async function (req, res) {
   const state = await myIdentifier.generate();
-  console.log(`generated state: ${state}`);
+  // console.log(`generated state: ${state}`);
   const queryParams = querystring.stringify({
     client_id: slack_client_id,
     response_type: "code",
@@ -17,7 +17,7 @@ const authenticationWithSlack = async function (req, res) {
     state: state,
     scope: scope,
   });
-  console.log(queryParams);
+  // console.log(queryParams);
   res.redirect(`https://slack.com/openid/connect/authorize?${queryParams}`);
 };
 
