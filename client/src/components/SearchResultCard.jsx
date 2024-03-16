05e07df367038bc3327c36f63eaa484353a1dd4c
mandy <109825538+m4ndycheung@users.noreply.github.com>
2024-03-16 18:49:18 +0000

    Passing down props needed for onClick and isSelected

--- client/src/components/SearchResultCard.jsx
+++ client/src/components/SearchResultCard.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 
-function SearchResultCard({ key, trackName, artistName, songURI, imageURL }) {
+function SearchResultCard({
+  key,
+  trackName,
+  artistName,
+  songURI,
+  imageURL,
+  isSelected,
+  onClick,
+}) {
   return (
-    <div key={key}>
+    <div
+      key={key}
+      className={`search-result-card ${isSelected ? "selected" : ""}`}
+      onClick={() => onClick(trackName, artistName, songURI, imageURL)}
+    >
       {trackName} by {artistName}
     </div>
   );
