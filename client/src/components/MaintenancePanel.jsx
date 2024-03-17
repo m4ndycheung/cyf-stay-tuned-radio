bbb9c55943b7dcf98b822c6de84aefc794c57a0c
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-03-17 23:41:00 +0000

    add button and Collapse component

--- client/src/components/MaintenancePanel.jsx
+++ client/src/components/MaintenancePanel.jsx
@@ -1,11 +1,29 @@
+import { useState } from "react";
+import Collapse from "react-bootstrap/Collapse";
 import SpotifyFirstRunButton from "./SpotifyFirstRunButton";
 import UpdateSpotifyPlaylistButton from "./UpdateSpotifyPlaylistButton";
 
 export default function MaintenancePanel({ userRole }) {
+  const [open, setOpen] = useState(false);
+
   return (
     <>
-      <UpdateSpotifyPlaylistButton userRole={userRole} />
-      <SpotifyFirstRunButton />
+      <button
+        type="submit"
+        className="btn btn-secondary m-3"
+        onClick={() => setOpen(!open)}
+        aria-controls="control-collapse-add-song-form"
+        aria-expanded={open}
+      >
+        Maintenance
+      </button>
+      <div className="rounded container bg-body-tertiary">
+        <Collapse in={open}>
+          <div>this is hidden</div>
+        </Collapse>
+        <UpdateSpotifyPlaylistButton userRole={userRole} />
+        <SpotifyFirstRunButton />
+      </div>
     </>
   );
 }
